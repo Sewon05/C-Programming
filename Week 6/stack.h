f4893e65e8e2bc24b7ef1b068f6a54071f96e1f5
Sewon05 <scottswpark@gmail.com>
2025-05-08 15:58:33 +0900

    Add files via upload

---
+++ Week 6/stack.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef STACK_H
+#define STACK_H
+#define MAX 100
+typedef struct {
+	int data[MAX];
+	int top;
+}Stack;
+
+void initStack(Stack* s);
+int isEmpty(Stack* s);
+void push(Stack* s, int value);
+int pop(Stack* s);
+int peek(Stack* s);
+void display(Stack* S);
+#endif //STACK_H
